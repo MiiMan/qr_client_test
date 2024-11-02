68b14977158336581e0e551feee88a3d6cbbc12a
Gennady Alheev <44393437+MiiMan@users.noreply.github.com>
2024-11-02 23:57:24 +0300

    Initial commit

--- lib/presentation/qr_page.dart
+++ lib/presentation/qr_page.dart
@@ -11,83 +11,35 @@ class QrPage extends StatefulWidget {
 }
 
 class _QrPageState extends State<QrPage> with WidgetsBindingObserver {
-  final MobileScannerController controller = MobileScannerController(
-    // required options for the scanner
-  );
 
-  StreamSubscription<Object?>? _subscription;
+  String data = 'initial data';
 
   void _handleBarcode(BarcodeCapture capture) {
-    print(capture.barcodes);
+    data = capture.barcodes[0].rawValue!;
   }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // Start listening to lifecycle changes.
-  //   WidgetsBinding.instance.addObserver(this);
-  //
-  //   // Start listening to the barcode events.
-  //   _subscription = controller.barcodes.listen(_handleBarcode);
-  //
-  //   // Finally, start the scanner itself.
-  //   unawaited(controller.start());
-  // }
-  //
-  // @override
-  // Future<void> dispose() async {
-  //   // Stop listening to lifecycle changes.
-  //   WidgetsBinding.instance.removeObserver(this);
-  //   // Stop listening to the barcode events.
-  //   unawaited(_subscription?.cancel());
-  //   _subscription = null;
-  //   // Dispose the widget itself.
-  //   super.dispose();
-  //   // Finally, dispose of the controller.
-  //   await controller.dispose();
-  // }
-  //
-  // @override
-  // void didChangeAppLifecycleState(AppLifecycleState state) {
-  //   // If the controller is not ready, do not try to start or stop it.
-  //   // Permission dialogs can trigger lifecycle changes before the controller is ready.
-  //   if (!controller.value.hasCameraPermission) {
-  //     return;
-  //   }
-  //
-  //   switch (state) {
-  //     case AppLifecycleState.detached:
-  //     case AppLifecycleState.hidden:
-  //     case AppLifecycleState.paused:
-  //       return;
-  //     case AppLifecycleState.resumed:
-  //     // Restart the scanner when the app is resumed.
-  //     // Don't forget to resume listening to the barcode events.
-  //       _subscription = controller.barcodes.listen(_handleBarcode);
-  //
-  //       unawaited(controller.start());
-  //     case AppLifecycleState.inactive:
-  //     // Stop the scanner when the app is paused.
-  //     // Also stop the barcode events subscription.
-  //       unawaited(_subscription?.cancel());
-  //       _subscription = null;
-  //       unawaited(controller.stop());
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SizedBox(
-          height: 200,
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: MobileScanner(
-              onDetect: _handleBarcode,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 200,
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: MobileScanner(
+                  onDetect: _handleBarcode,
+                ),
+              ),
             ),
-          ),
-        ),
+
+            SizedBox(height: 100,),
+
+            Text(data)
+          ],
+        )
       ),
     );
   }
